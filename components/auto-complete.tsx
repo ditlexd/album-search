2f1025a0fd0674e418b90199c643e9492af3100a
Ditlexd <ditlef.diseth@gmail.com>
2022-06-26 13:45:36 +0200

    Finish first iteration of autocomplete and task 1

--- components/auto-complete.tsx
+++ components/auto-complete.tsx
@@ -1,35 +1,27 @@
 import { useState } from "react";
 import { Combobox } from "@headlessui/react";
 
-const people = [
-  "Prince",
-  "Prince & The Revolution",
-  "Prince Royce",
-  "Eminem",
-  "Nirvana",
-];
-
 type Props = {
   onInputChange: (input: string) => void;
+  items: { displayValue: string }[];
 };
-export default function AutoComplete({ onInputChange }: Props) {
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
-  const [query, setQuery] = useState("");
+export default function AutoComplete({ onInputChange, items }: Props) {
+  const [selectedPerson, setSelectedPerson] = useState(null);
 
-  const filteredPeople =
-    query === ""
-      ? people
-      : people.filter((person) => {
-          return person.toLowerCase().includes(query.toLowerCase());
-        });
+  async function inputChanged(input: string) {
+    onInputChange(input);
+  }
 
   return (
     <div className={"flex w-full gap-4"}>
-      <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+      <Combobox
+        value={selectedPerson}
+        onChange={(person) => setSelectedPerson(person)}
+      >
         <div className={"relative w-full"}>
           <Combobox.Input
             className={"h-20 w-full bg-gray-800 pl-2 text-3xl text-gray-300"}
-            onChange={(event) => setQuery(event.target.value)}
+            onChange={(event) => inputChanged(event.target.value)}
           />
           <Combobox.Options>
             <div
@@ -37,11 +29,11 @@ export default function AutoComplete({ onInputChange }: Props) {
             >
               <p className={"pl-1"}>Search results</p>
               <div>
-                {filteredPeople.map((person) => (
-                  <Combobox.Option key={person} value={person}>
+                {items.map(({ displayValue }, i) => (
+                  <Combobox.Option key={displayValue + i} value={displayValue}>
                     {({ selected, active }) => (
                       <p className={active ? "bg-white" : "" + "pl-3"}>
-                        {person}
+                        {displayValue}
                       </p>
                     )}
                   </Combobox.Option>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,39 @@
 import type { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AutoComplete from "../components/auto-complete";
 
+const people = [
+  "Prince",
+  "Prince & The Revolution",
+  "Prince Royce",
+  "Eminem",
+  "Nirvana",
+];
+
 const Home: NextPage = () => {
-  fetch("/api/hello");
+  const [artists, setArtists] = useState<string[]>([]);
+
+  async function fetchArtists(query: string) {
+    console.log("Fetching");
+    const fetchedArtists = await fetch("/api/artists/" + query).then((res) =>
+      res.json()
+    );
+
+    console.log(fetchedArtists);
+
+    setArtists(fetchedArtists.artists);
+  }
+
   return (
     <div className={"grid h-screen w-screen grid-cols-6 bg-black pt-10"}>
       <div className={"col-span-4 col-start-2"}>
-        <AutoComplete onInputChange={(s) => {}} />;
+        <AutoComplete
+          onInputChange={fetchArtists}
+          items={artists.map((p) => {
+            return { displayValue: p };
+          })}
+        />
+        ;
       </div>
     </div>
   );
